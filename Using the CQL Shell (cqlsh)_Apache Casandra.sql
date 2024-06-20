f4a0bb189e3cc18b45615df2e628f35664c177c8
Ardalan <168172562+dataengineer773@users.noreply.github.com>
2024-06-21 01:05:52 +0330

    Add files via upload

---
+++ Using the CQL Shell (cqlsh)_Apache Casandra.sql
@@ -0,0 +1,17 @@
+# Run the following command on the newly opened terminal.
+cqlsh --username cassandra --password PASSWORD
+
+# This command will show the details of the server you have connected to.
+show host
+
+# This command will show the version of the Cassandra server
+show version
+
+# This command will print a list of the keyspaces present on the server.
+describe keyspaces
+
+# This command will clear the cqlsh screen
+cls
+
+# On the cqlsh run the below command.
+exit
